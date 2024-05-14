7311d12db0fa6b0f6b4c8b9b591aef99bc181be0
Daniel Veerkamp <danielveerkamp@live.com>
2024-05-14 15:58:06 +1200

    Add project files.

---
+++ Whiteness-Toggle/Mod.cs
@@ -0,0 +1,195 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using UnityEngine;
+using UnityEngine.Windows;
+using UnityEngine.InputSystem;
+using Colossal.Serialization.Entities;
+using Game.Rendering;
+using Game.Simulation;
+using Game.UI;
+using System.Collections.Generic;
+using System.IO;
+using Game.Tools;
+using Game.Prefabs;
+using HarmonyLib;
+using static Game.UI.InGame.InfoviewsUISystem;
+
+
+
+
+
+namespace Whiteness_Toggle
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(Whiteness_Toggle)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private static Harmony _harmony;
+
+        
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+
+            _harmony = new Harmony("com.daniel.whitenessstoggle.whitenesstoggle");
+            _harmony.PatchAll();
+            log.Info(nameof(OnLoad));
+
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            //updateSystem.UpdateAt<WhitenessSystem>(SystemUpdatePhase.MainLoop);
+            updateSystem.UpdateAt<WhitenessSystem>(SystemUpdatePhase.Rendering);
+
+
+
+        }
+
+        public void OnDispose()
+        {
+            _harmony?.UnpatchAll("com.daniel.whitenessstoggle.whitenesstoggle");
+            log.Info(nameof(OnDispose));
+        }
+
+       
+
+
+    }
+
+    public partial class WhitenessSystem : GameSystemBase
+    {
+
+        public Game.Tools.ToolSystem _toolSystem;
+
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            SetupKeybinds();
+        }
+
+       
+
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+            base.OnGameLoadingComplete(purpose, mode);
+
+            if (!mode.IsGameOrEditor())
+                return;
+
+            if (_toolSystem == null)
+            {
+                _toolSystem = World.GetExistingSystemManaged<Game.Tools.ToolSystem>();
+
+            }
+            else
+            {
+                Mod.log.Info("TOOL SYSTEM IS NULL");
+            }
+        }
+
+        private void SetupKeybinds()
+        {
+            var inputAction = new InputAction("_WhitenessToggle");
+            inputAction.AddCompositeBinding("ButtonWithOneModifier")
+                .With("Modifier", "<Keyboard>/shift")
+                .With("Button", "<Keyboard>/w");
+            inputAction.Enable();
+            inputAction.performed += OnShiftWPressed;
+            Mod.log.Info("Keybinds set up correctly.");
+
+        }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+        public bool isPressed = false;
+        public bool whitenessToggle;
+
+
+        private void OnShiftWPressed(InputAction.CallbackContext context)
+        {
+            isPressed = !isPressed; // Toggle the isPressed boolean
+            Mod.log.Info($"Whiteness set to {isPressed}"); // Log the updated value
+
+
+            TriggerUpdate("UseStickyWhiteness");
+
+            
+            Mod.log.Info("Keybinds (Shift + W) pressed.");
+
+        }
+        public InfoviewPrefab lastActiveInfoView;
+
+        public void TriggerUpdate(string property)
+        {
+            switch (property)
+            {
+                case "UseStickyWhiteness":
+                    if (isPressed)
+                        Shader.SetGlobalInt("colossal_InfoviewOn", _toolSystem.activeInfoview?.active == true &&
+                            whitenessToggle ? 1 : 0);
+                    Mod.log.Info("Toggled Whiteness");
+
+                    break;
+
+            }
+        }
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+       //public bool hasTriggered = false;
+
+        protected override void OnUpdate()
+        {
+
+           /* if (_toolSystem != null && _toolSystem.activeInfoview != lastActiveInfoView)
+            {
+                lastActiveInfoView = _toolSystem.activeInfoview; // Update the last active info view
+                hasTriggered = false; // Reset the trigger flag when the info view changes
+            }
+
+            if (_toolSystem != null && _toolSystem.activeInfoview?.active == true && !hasTriggered)
+            {
+                TriggerUpdate("UseStickyWhiteness");
+                hasTriggered = true; // Set the flag after executing once
+            }*/
+        }
+        
+        
+
+
+
+    }
+
+    
+}
+
+
+
+
---
+++ Whiteness-Toggle/Patches.cs
@@ -0,0 +1,32 @@
+﻿using Game.Tools;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using UnityEngine;
+using Whiteness_Toggle;
+
+[HarmonyPatch(typeof(ToolSystem), "UpdateInfoviewColors")]
+public static class InfoviewUpdatePatch
+{
+    static WhitenessSystem _whitenesSystem;
+
+    static void Postfix(ToolSystem __instance)
+    {
+        // Get the WhitenessSystem instance
+        if (_whitenesSystem == null)
+        {
+            _whitenesSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<WhitenessSystem>();
+        }
+
+        if (__instance.activeInfoview?.active == true && _whitenesSystem.isPressed == true)
+        {
+            // Update the shader when the Infoview is updated
+            Shader.SetGlobalInt("colossal_InfoviewOn", _whitenesSystem.whitenessToggle ? 1 : 0);
+            Mod.log.Info("UPDATED FROM HARMONEY PATCH");
+        }
+    }
+}
