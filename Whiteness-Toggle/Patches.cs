006932c38e80c70dca896b73178c2a3791318ba0
Daniel Veerkamp <danielveerkamp@live.com>
2024-05-17 13:02:02 +1200

    # Patch V1.0.4 * Added more colour presets. * Fixed an issue causing very bright neon-like roads. * KNOWN ISSUE: Roads are still Neon.... (sorry I guess!)

--- Whiteness-Toggle/Patches.cs
+++ Whiteness-Toggle/Patches.cs
@@ -31,7 +31,112 @@ public static class InfoviewUpdatePatch
         {
             // Update the shader when the Infoview is updated
             Shader.SetGlobalInt("colossal_InfoviewOn", _whitenesSystem.whitenessToggle ? 1 : 0);
+           
             Mod.log.Info("UPDATED FROM HARMONEY PATCH");
         }
+        if (__instance.activeInfoview?.active == true && Mod.m_Setting.ToggleOverlay == false)
+        {
+            // Update the shader when the Infoview is updated
+            __instance.activeInfoview.m_DefaultColor = Color.white;
+           
+
+            Mod.log.Info("UPDATED FROM HARMONEY PATCH");
+        } 
+        else if (__instance.activeInfoview?.active == true && Mod.m_Setting.ToggleOverlay == true)
+
+
+        {
+            Vector4 myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);  // RGBA values normalized
+
+
+            if (Mod.m_Setting.m_Protanopia)
+            {
+                Mod.m_Setting.Red = 255;
+                Mod.m_Setting.Green = 128;
+                Mod.m_Setting.Blue = 0;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_Deuteranopia)
+            {
+                Mod.m_Setting.Red = 255;
+                Mod.m_Setting.Green = 129;
+                Mod.m_Setting.Blue = 0;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_Tritanopia)
+            {
+                Mod.m_Setting.Red = 129;
+                Mod.m_Setting.Green = 64;
+                Mod.m_Setting.Blue = 64;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_lightBlue)
+            {
+                Mod.m_Setting.Red = 5;
+                Mod.m_Setting.Green = 5;
+                Mod.m_Setting.Blue = 15;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_lightGreen)
+            {
+                Mod.m_Setting.Red = 5;
+                Mod.m_Setting.Green = 15;
+                Mod.m_Setting.Blue = 5;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_lightRed)
+            {
+                Mod.m_Setting.Red = 15;
+                Mod.m_Setting.Green = 5;
+                Mod.m_Setting.Blue = 5;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_lightYellow)
+            {
+                Mod.m_Setting.Red = 19;
+                Mod.m_Setting.Green = 16;
+                Mod.m_Setting.Blue = 0;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else if (Mod.m_Setting.m_lightPink)
+            {
+                Mod.m_Setting.Red = 6;
+                Mod.m_Setting.Green = 0;
+                Mod.m_Setting.Blue = 8;
+
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+            }
+            else
+            {
+                // Default color (no colorblindness adjustment)
+                myColor = new Vector4(Mod.m_Setting.Red / 255f, Mod.m_Setting.Green / 255f, Mod.m_Setting.Blue / 255f, 0.5f);
+                
+            }
+            
+
+            Vector4[] colorArray = new Vector4[10];
+            for (int i = 0; i < colorArray.Length; i++)
+            {
+                colorArray[i] = myColor;
+            }
+
+            Shader.SetGlobalVectorArray("colossal_InfomodeColors", colorArray);
+
+
+            Mod.log.Info("UPDATED FROM HARMONEY PATCH - COLOURS");
+
+
+        }
+
+
     }
+
+  
 }
--- Whiteness-Toggle/Settings.cs
+++ Whiteness-Toggle/Settings.cs
@@ -10,19 +10,37 @@ using Whiteness_Toggle;
 namespace Whiteness_Toggle
 {
     [FileLocation(nameof(Whiteness_Toggle))]
-    [SettingsUIGroupOrder(kButtonGroup, kToggleGroup)]
-    [SettingsUIShowGroupName(kButtonGroup, kToggleGroup)]
+    [SettingsUIGroupOrder(kToggleGroup, kCustomColours, kButtonGroup, kButtonGroup2)]
+    [SettingsUIShowGroupName(kToggleGroup, kCustomColours, kButtonGroup, kButtonGroup2)]
     public class Setting : ModSetting
     {
 
         private WhitenessSystem _system;
         private Mod _mod;
-        public const string kSection = "Main";
+        private bool currentToggle;
 
-        public const string kButtonGroup = "Button";
+        public const string kSection = "Main";
+        public const string kSection2 = "Secondary";
+        public const string kSection3 = "Presets";
         public const string kToggleGroup = "Toggle";
-        public const string kSliderGroup = "Slider";
-        public const string kDropdownGroup = "Dropdown";
+        public const string kCustomColours = "Custom Colours";
+        public const string kButtonGroup = "Button";
+        public const string kButtonGroup2 = "Button1";
+        public const string kPresets = "Button2";
+        public string ColorBlindnessType;
+        public bool m_Protanopia;
+        public bool m_Deuteranopia;
+        public bool m_Tritanopia;
+        public bool m_TurnOff;
+        public bool m_lightBlue;
+        public bool m_lightGreen;
+        public bool m_lightRed;
+        public bool m_lightPink;
+        public bool m_lightYellow;
+
+
+
+
 
         public Setting(IMod mod, WhitenessSystem system) : base(mod)
         {
@@ -31,29 +49,93 @@ namespace Whiteness_Toggle
         }
         public override void SetDefaults()
         {
-            //throw new System.NotImplementedException();
             ToggleWhiteness = false;
+            currentToggle = false;
+            TurnOff = true;
+            m_Protanopia = false;
+            m_Deuteranopia = false;
+            m_Tritanopia = false;
+            m_TurnOff = true;
+            m_lightBlue = false;
+            m_lightGreen = false;
+            m_lightRed = false;
         }
 
-        public override void Apply()
-        {
-            base.Apply();
-            _system.TriggerUpdate("UseStickyWhiteness");
-            
-        }
 
+        
 
 
         [SettingsUISection(kSection, kToggleGroup)]
         public bool ToggleWhiteness { get; set; }
 
-        
-        
-        
+        [SettingsUISection(kSection, kToggleGroup)]
+        public bool ToggleOverlay { get; set; }
+
+
+
+        [SettingsUISection(kSection, kCustomColours)]
+        public bool EnableCustomColours { set { m_TurnOff = true; m_Deuteranopia = false; m_Tritanopia = false; m_Protanopia = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; } }
+
+
+        [SettingsUISlider(min = 0, max = 255, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
+        [SettingsUISection(kSection, kCustomColours)]
+        public int Red { get; set; }
+
+        [SettingsUISlider(min = 0, max = 255, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
+        [SettingsUISection(kSection, kCustomColours)]
+        public int Green { get; set; }
+
+        [SettingsUISlider(min = 0, max = 255, step = 1, scalarMultiplier = 1, unit = Unit.kInteger)]
+        [SettingsUISection(kSection, kCustomColours)]
+        public int Blue { get; set; }
+
+        [SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kFloatTwoFractions)]
+        [SettingsUISection(kSection, kCustomColours)]
+        public int Alpha { get; set; }
 
         
+        [SettingsUISection(kSection2, kButtonGroup)]
+        public bool Protanopia { set { m_Protanopia = true; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; } }
 
-    
+        [SettingsUISection(kSection2, kButtonGroup)]
+        public bool Deuteranopia { set { m_Deuteranopia = true; m_Protanopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; } }
+
+        [SettingsUISection(kSection2, kButtonGroup)]
+        public bool Tritanopia { set { m_Tritanopia = true; m_Deuteranopia = false; m_Protanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; } }
+
+        [SettingsUISection(kSection2, kButtonGroup)]
+        public bool TurnOff { set { m_TurnOff = true; m_Deuteranopia = false; m_Tritanopia = false; m_Protanopia = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; } }
+
+
+        [SettingsUISection(kSection3, kButtonGroup2)]
+        public bool LightBlue { set { m_Protanopia = false; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = true; m_lightGreen = false; m_lightRed = false; } }
+
+        [SettingsUISection(kSection3, kButtonGroup2)]
+        public bool LightGreen { set { m_Protanopia = false; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = true; m_lightRed = false; } }
+
+        [SettingsUISection(kSection3, kButtonGroup2)]
+        public bool LightRed { set { m_Protanopia = false; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = true; } }
+
+        [SettingsUISection(kSection3, kButtonGroup2)]
+        public bool LightYellow { set { m_Protanopia = false; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; m_lightPink = false; m_lightYellow = true; } }
+
+        [SettingsUISection(kSection3, kButtonGroup2)]
+        public bool LightPink { set { m_Protanopia = false; m_Deuteranopia = false; m_Tritanopia = false; m_TurnOff = false; m_lightBlue = false; m_lightGreen = false; m_lightRed = false; m_lightYellow = false; m_lightPink = true; } }
+
+        public override void Apply()
+        {
+            base.Apply();
+
+            if (ToggleWhiteness != currentToggle)
+            {
+                _system.TriggerUpdate("UseStickyWhiteness");
+            }
+            
+
+        }
+
+
+
     }
 
     public class LocaleEN : IDictionarySource
@@ -67,18 +149,56 @@ namespace Whiteness_Toggle
         {
             return new Dictionary<string, string>
             {
-                { m_Setting.GetSettingsLocaleID(), "Mod settings sample" },
-                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
-
-                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
+                { m_Setting.GetSettingsLocaleID(), "Whiteness Toggle" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main Options" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection2), "Colour Blindness Presets" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection3), "Normal Presets" },
                 { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Options" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Sliders" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kCustomColours), "Custom Colours" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Colour Blind Presets" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup2), "Normal Presets" },
 
 
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.EnableCustomColours)), "Enable Custom Colours" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.EnableCustomColours)), $"Sets mode to enable custom colours" },
 
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleWhiteness)), "Toggle Whiteness Off" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.ToggleWhiteness)), $"(Unticked is whiteness on and ticked is whiteness off. Use this to enable/disable Whiteness, can also be done with Shift + W" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Red)), "Red" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Red)), $"When the overlay is turned on, this changes the red value" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Green)), "Green" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Green)), $"When the overlay is turned on, this changes the green value" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Blue)), "Blue" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Blue)), $"When the overlay is turned on, this changes the blue value" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Alpha)), "Alpha (Unavailable)" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Alpha)), $"Currently Unavailable - When the overlay is turned on, this changes the opacity" },
+                
+                
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleWhiteness)), "Disable Whiteness" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.ToggleWhiteness)), $"Use this to enable/disable Whiteness, can also be done with Shift + W" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ToggleOverlay)), "Use Custom Overlay" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.ToggleOverlay)), $"Use this to enable a custom Overlay using the sliders below or the colour blind settings." },
+                
+                
+                
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Protanopia)), "Protanopia" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Protanopia)), $"Sets mode to Protanopia" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Deuteranopia)), "Deuteranopia" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Deuteranopia)), $"Sets mode to Deuteranopia" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Tritanopia)), "Tritanopia" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Tritanopia)), $"Sets mode to Tritanopia" },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.TurnOff)), "Turn Off Colour Blind" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.TurnOff)), $"Turns off colour blind mode." },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LightBlue)), "Light Blue" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.LightBlue)), $"Sets mode to Light Blue." },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LightGreen)), "Light Green" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.LightGreen)), $"Sets mode to Light Green." },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LightRed)), "Light Red" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.LightRed)), $"Sets mode to Light Red." },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LightYellow)), "Light Yellow" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.LightYellow)), $"Sets mode to Light Yellow." },
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.LightPink)), "Light Pink" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.LightPink)), $"Sets mode to Light Pink." },
 
 
             };
